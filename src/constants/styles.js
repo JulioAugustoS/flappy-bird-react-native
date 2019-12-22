2893dba66a3fd82b122792011f1c76c9e3c265fa
Julio Augusto <julioams@icloud.com>
2019-12-22 16:13:40 -0300

    change styles

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,9 +1,19 @@
 import { StyleSheet } from 'react-native';
+import Constants from '~/constants';
 
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e1e1e1'
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: Constants.MAX_WIDTH,
+    height: Constants.MAX_HEIGHT
   },
   gameContainer: {
     position: 'absolute',
@@ -33,7 +43,27 @@ const Styles = StyleSheet.create({
   },
   gameOverText: {
     color: 'white',
-    fontSize: 48
+    fontSize: 48,
+    fontFamily: '04b_19'
+  },
+  gameOverSubText: {
+    color: 'white',
+    fontSize: 24,
+    fontFamily: '04b_19'
+  },
+  score: {
+    position: 'absolute',
+    color: 'white',
+    fontSize: 72,
+    top: 50,
+    left: Constants.MAX_WIDTH / 2 - 30,
+    textShadowColor: '#444444',
+    textShadowOffset: {
+      width: 2,
+      height: 2
+    },
+    textShadowRadius: 2,
+    fontFamily: '04b_19'
   }
 });
 
