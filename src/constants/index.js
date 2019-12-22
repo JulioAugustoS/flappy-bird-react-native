ad295f86facdd85b2cc4b18ab5517b6d3ecb1781
Julio Augusto <julioams@icloud.com>
2019-12-22 13:51:02 -0300

    created constants file

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+import { Dimensions } from 'react-native';
+
+const Constants = {
+  MAX_WIDTH: Dimensions.get("screen").width,
+  MAX_HEIGHT: Dimensions.get("screen").height,
+  GAP_SIZE: 200,
+  PIPE_WIDTH: 100
+}
+
+export default Constants;
