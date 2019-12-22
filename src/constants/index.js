fe27fc83e7f4572c8ef58040afd7ba62fa079c02
Julio Augusto <julioams@icloud.com>
2019-12-22 16:13:33 -0300

    change constantes

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,8 +3,10 @@ import { Dimensions } from 'react-native';
 const Constants = {
   MAX_WIDTH: Dimensions.get("screen").width,
   MAX_HEIGHT: Dimensions.get("screen").height,
-  GAP_SIZE: 200,
-  PIPE_WIDTH: 100
+  GAP_SIZE: 320,
+  PIPE_WIDTH: 100,
+  BIRD_WIDTH: 50,
+  BIRD_HEIGHT: 41
 }
 
 export default Constants;
