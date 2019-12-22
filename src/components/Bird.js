9a83ee97f70d9f17daf976000cdb989fa2874318
Julio Augusto <julioams@icloud.com>
2019-12-22 16:12:05 -0300

    change bird component for add asset

--- src/components/Bird.js
+++ src/components/Bird.js
@@ -1,24 +1,45 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Animated } from 'react-native';
 
-const Bird = (props) => {
-  const width = props.size[0];
-  const height = props.size[1];
-  const x = props.body.position.x - width / 2;
-  const y = props.body.position.y - height / 2;
+// Images
+import Images from '~/assets/Images';
 
-  return (
-    <View
-      style={{
-        position: 'absolute',
-        top: y,
-        left: x,
-        width: width,
-        height: height,
-        backgroundColor: props.color
-      }}
-    />
-  );
+class Bird extends React.Component {
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(this.props.body.velocity.y);
+  }
+
+  render() {
+    const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
+    const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
+    const x = this.props.body.position.x - width / 2;
+    const y = this.props.body.position.y - height / 2;
+
+    this.animatedValue.setValue(this.props.body.velocity.y);
+    let rotation = this.animatedValue.interpolate({
+      inputRange: [-10, 0, 10, 20],
+      outputRange: ['-20deg', '0deg', '15deg', '45deg'],
+      extrapolate: 'clamp'
+    })
+
+    let image = Images['bird' + this.props.pose];
+
+    return (
+      <Animated.Image
+        style={{
+            position: "absolute",
+            left: x,
+            top: y,
+            width: width,
+            height: height,
+            transform: [{ rotate: rotation }]
+        }}
+        resizeMode="stretch"
+        source={image}
+      />
+    );
+  }
 }
 
 export default Bird;
