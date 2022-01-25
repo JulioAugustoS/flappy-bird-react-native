71c6deec4f537b2b2ecb2aa081235aafa734970f
Julio Augusto <julioams@icloud.com>
2022-01-25 18:45:56 -0300

    feat: remove import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
-// import '~/config/ReactotronConfig';
 
 // Redux
-import { Provider } from 'react-redux'; 
+import { Provider } from 'react-redux';
 import store from './store';
 
 import Routes from './routes';
